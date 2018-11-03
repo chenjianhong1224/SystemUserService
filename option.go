2e0bf66f193d5630d2bd65a31704127167d90d88
chenjianhong <chw_k1@163.com>
2018-11-04 00:23:01 +0800

    12

--- option.go
+++ option.go
@@ -13,7 +13,7 @@ func newOptions() *options {
 
 func (o *options) InstallFlags() {
 	flag.BoolVar(&o.version, "version", false, "show the app version")
-	flag.StringVar(&o.configFile, "config", "conf/server.yaml", "set config file")
+	flag.StringVar(&o.configFile, "config", "../conf/server.yaml", "set config file")
 
 	flag.Parse()
 }
